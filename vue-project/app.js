d2f4de2aee290fd505593c212ef34570ada6a497
KoCSience <90507944+KoCSience@users.noreply.github.com>
2024-03-12 11:34:07 +0100

    add express

---
+++ vue-project/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+
+const apiRouter = require('./routes/api.js')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  session({
+    cookie: {
+      //fixer la session
+      path: '/',
+      httpOnly: false,
+      maxAge: 24 * 60 * 60 * 1000
+    },
+    secret: 'grehjznejzkhgjrez',
+    saveUninitialized: false,
+    resave: false
+  })
+)
+app.use(express.static(path.join(__dirname, '../client')))
+
+app.use('/api/', apiRouter)
+
+module.exports = app
