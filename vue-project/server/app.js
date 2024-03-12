3b714832a4b6ed98bbcecee885d59c43f23039d8
KoCSience <90507944+KoCSience@users.noreply.github.com>
2024-03-12 12:15:09 +0100

    server追加

--- vue-project/server/app.js
+++ vue-project/server/app.js
@@ -25,7 +25,7 @@ app.use(
     resave: false
   })
 )
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api/', apiRouter)
 
